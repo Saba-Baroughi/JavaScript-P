2ea2daffbc1a768433220c0a6f5e3fc0635abbc3
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-12-10 12:40:19 +0000

    fetch-post

---
+++ API-post.js
@@ -0,0 +1,23 @@
+const $ = document;
+const firstname = $.querySelector(".firstname");
+const lastname = $.querySelector(".lastname");
+const password = $.querySelector(".password");
+const button = $.querySelector("button");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("register");
+  let userData = {
+    firstname: firstname.value,
+    lastname: lastname.value,
+    password: password.value,
+}
+
+fetch('http://localhost:3000/api/users', {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify(userData)
+})
+.then(res => console.log(res))
+});
---
+++ assets/script.js
@@ -0,0 +1,3 @@
+const API_KEY='fZupyGdcSOFuuUGvIxDGwJL11AM';
+const API_URL='https://ci-jshint.herokuapp.com/api'
+const resultsModal=new bootstarp.Modal(document.getElementById('resultsModel'))
