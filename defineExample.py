778317eb43e8aa004786f52384f3799c55cb412a
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-12-16 10:24:34 +0000

    example.py

--- defineExample.py
+++ defineExample.py
@@ -1,4 +1,4 @@
-numbers = [12, 45, 60, 87, 999, 200, 84, 42, 87, 77, 2, 3, 77, 99, 20]
+numbers = [12, 45, 60, 87, 999, 200, 84, 42, 87, 77, 2, 3, 77, 99, 20,67,78]
 def get_odd_nums(list_of_nums):
     print(list_of_nums)
     new_list=[]
@@ -7,6 +7,7 @@ def get_odd_nums(list_of_nums):
      if num %2!=0:
         new_list.append(num)
        
-    print(new_list)
-get_odd_nums(numbers)
+    return new_list
+result=get_odd_nums(numbers)
+print(result)
     
